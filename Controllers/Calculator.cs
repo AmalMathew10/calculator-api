be69544d3cd0162ec39a165d793a401100f26b5f
AmalMathew10 <158222372+AmalMathew10@users.noreply.github.com>
2024-02-01 00:02:13 +0000

    calculator

---
+++ Controllers/Calculator.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class Calculator : ControllerBase
+{
+    [HttpGet("Add")]
+    public ActionResult<decimal> Add(decimal number1, decimal number2)
+    {
+        return number1 + number2;
+    }
+    [HttpGet("Subtract")]
+    public ActionResult<decimal> Subtract(decimal number1, decimal number2)
+    {
+        return number1 - number2;
+    }
+    [HttpGet("Multiply")]
+    public ActionResult<decimal> Multiply(decimal number1, decimal number2)
+    {
+        return number1 * number2;
+    }
+    [HttpGet("Divide")]
+    public ActionResult<decimal> Divide(decimal number1, decimal number2)
+    {
+        if(number2==0)
+        {
+            return BadRequest("Can't divided by zero");
+        }
+        return number1 / number2;
+    }
+    [HttpGet("Modulo")]
+    public ActionResult<decimal> Modulo(decimal number1, decimal number2)
+    {
+        if(number2==0)
+        {
+            return BadRequest("Can't find out modulo");
+        }
+        return number1 % number2;
+    }
+}
